ea1c26316724489f3b47f61ad04764dd04aef636
BarsatKhadka <khadkabarsat598@gmail.com>
2024-11-28 14:22:13 -0600

    Initialized project with spring initializr

---
+++ Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/GithubRepositoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Barsat.Github.Repository.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoryManagementApplication.class, args);
+	}
+
+}
---
+++ Github-Repository-Management/src/test/java/com/Barsat/Github/Repository/Management/GithubRepositoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barsat.Github.Repository.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
