563e60207eb7d45dd76e49097b2570c10499f3ce
BarsatKhadka <khadkabarsat598@gmail.com>
2024-12-15 17:12:32 -0600

    update: Frontend boiler plate and setup with NextUi , tailwind , vite with typescript

---
+++ grms-frontend/src/components/Home.tsx
@@ -0,0 +1,10 @@
+// pages/index.js
+import { Button } from '@nextui-org/react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <Button>Test NextUI</Button>
+    </div>
+  );
+}
---
+++ grms-frontend/src/components/Login.tsx
@@ -0,0 +1,10 @@
+export const Login = () =>{
+    const githubLogin = () =>{
+        window.location.href = "http://localhost:8080/oauth2/authorization/github"
+    }
+    return(
+        <div>
+            <button onClick={githubLogin}>Login with github</button>
+        </div>
+    )
+}
