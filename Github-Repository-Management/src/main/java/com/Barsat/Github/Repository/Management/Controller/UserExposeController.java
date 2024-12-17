1dbbea304b280b562df9ca6fadf79b12cf53cd4c
BarsatKhadka <khadkabarsat598@gmail.com>
2024-12-16 18:30:10 -0600

    add: isAuthenticated endpoint to let frontend know if user is authenticated and setup protected routes

--- Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/Controller/UserExposeController.java
+++ Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/Controller/UserExposeController.java
@@ -1,5 +1,8 @@
 package com.Barsat.Github.Repository.Management.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +14,19 @@ import java.util.Map;
 @RestController
 @RequestMapping("/easyrepo/user")
 public class UserExposeController {
+
+    @GetMapping("/isAuthenticated")
+    public ResponseEntity<Map<String , Boolean>> isAuthenticated(Authentication authentication) {
+        boolean isAuthenticated = authentication != null  && authentication.isAuthenticated() ;
+        if (isAuthenticated) {
+            System.out.println("isAuthenticated");
+            return ResponseEntity.ok(Map.of("isAuthenticated", true));
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("isAuthenticated", false));
+        }
+
+    }
 
     @GetMapping
     public Map<String,Object> getUser(@AuthenticationPrincipal OAuth2User principal) {
