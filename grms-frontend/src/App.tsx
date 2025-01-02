5bec6f46dc026343c8754563eaacf76d0bf10114
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-01 22:16:20 -0600

    add: Login page UI

--- grms-frontend/src/App.tsx
+++ grms-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import{QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 //Component imports
-import { Login } from "./components/AuthComponents/Login"
+import  Login  from "./components/AuthComponents/Login"
 import {Home} from "./components/HomeComponents/Home"
 import {DashBoard} from "./components/DashBoard"
 import ProtectedRoutes from "./ProtectedRoutes/ProtectedRoutes"
--- grms-frontend/src/components/AuthComponents/Login.tsx
+++ grms-frontend/src/components/AuthComponents/Login.tsx
@@ -1,14 +1,71 @@
 
+"use client";
 
-export const Login = () =>{
+import {Button, Input, Checkbox, Link, Divider} from "@nextui-org/react";
+import {Icon} from "@iconify/react";
 
-    const githubLogin = () =>{
-        window.location.href = "http://localhost:8080/oauth2/authorization/github" 
-        
-    }
-    return(
-        <div>
-            <button onClick={() => {githubLogin() }}>Login with github</button>
+
+
+export default function Login() {
+
+  const githubLogin = () =>{
+    window.location.href = "http://localhost:8080/oauth2/authorization/github" 
+    
+}
+
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large">
+        <div className="flex flex-col items-center pb-6">
+            {/* {icon here} */}
+          <p className="text-xl font-medium">Welcome</p>
+          <p className="text-small text-default-500">Let's wrap it up.</p>
         </div>
-    )
+        <form className="flex flex-col gap-3" onSubmit={(e) => e.preventDefault()}>
+          <div className="flex flex-col">
+            <Input
+              isRequired
+              classNames={{
+                base: "-mb-[2px]",
+                inputWrapper:
+                  "rounded-b-none data-[hover=true]:z-10 group-data-[focus-visible=true]:z-10",
+              }}
+              label="Github Username"
+              name="username"
+              placeholder="Enter your username"
+              type="text"
+              variant="bordered"
+            />
+    
+        
+    
+          </div>
+          <Checkbox isRequired className="py-4" size="sm">
+            I agree with the&nbsp;
+            <Link href="#" size="sm">
+              Contrubution Policy
+            </Link>
+          </Checkbox>
+          <Button color="primary" type="submit">
+            Join the Codebase
+          </Button>
+        </form>
+        <div className="flex items-center gap-4 py-2">
+          <Divider className="flex-1" />
+          <p className="shrink-0 text-tiny text-default-500">OR</p>
+          <Divider className="flex-1" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Button
+            startContent={<Icon className="text-default-500" icon="fe:github" width={24} />}
+            onPress = {() => {githubLogin()}}
+            variant="bordered"
+          >
+            Log in with Github
+          </Button>
+        </div>
+    
+      </div>
+    </div>
+  );
 }
