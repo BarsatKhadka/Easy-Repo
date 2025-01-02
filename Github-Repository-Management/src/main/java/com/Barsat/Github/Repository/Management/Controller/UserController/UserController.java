c2e9f7768845b1c43ede8e4b26d5aa9c342b905e
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-02 12:12:31 -0600

    add : supporting controllers and services for data for navbar after authentication

--- Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/Controller/UserController/UserController.java
+++ Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/Controller/UserController/UserController.java
@@ -1,4 +1,24 @@
 package com.Barsat.Github.Repository.Management.Controller.UserController;
 
+import com.Barsat.Github.Repository.Management.Models.TheUser;
+import com.Barsat.Github.Repository.Management.Service.UserService.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/easyrepo/user/getUserDetails")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public TheUser getUserDetails(){
+        return userService.getUser();
+    }
+
 }
--- Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/Service/UserService/UserService.java
+++ Github-Repository-Management/src/main/java/com/Barsat/Github/Repository/Management/Service/UserService/UserService.java
@@ -1,4 +1,23 @@
 package com.Barsat.Github.Repository.Management.Service.UserService;
 
+import com.Barsat.Github.Repository.Management.Models.TheUser;
+import com.Barsat.Github.Repository.Management.Repository.UserRepo;
+import com.Barsat.Github.Repository.Management.Service.UtilityService.GetAuthenticatedUserName;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final GetAuthenticatedUserName getAuthenticatedUserName;
+    private final UserRepo userRepo;
+
+    public UserService(GetAuthenticatedUserName getAuthenticatedUserName , UserRepo userRepo) {
+        this.getAuthenticatedUserName = getAuthenticatedUserName;
+        this.userRepo = userRepo;
+    }
+    public TheUser getUser() {
+        String username = getAuthenticatedUserName.getUsername();
+        TheUser user = userRepo.findByUsername(username);
+        return user;
+    }
 }
