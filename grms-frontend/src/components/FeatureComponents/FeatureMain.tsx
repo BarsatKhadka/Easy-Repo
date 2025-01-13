946a9e9b06c7fc472397d1613b66248cd2ae4645
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-13 10:48:59 -0600

    add: Contributors section UI

--- grms-frontend/src/components/FeatureComponents/FeatureMain.tsx
+++ grms-frontend/src/components/FeatureComponents/FeatureMain.tsx
@@ -2,19 +2,20 @@ import {Textarea , Button} from "@nextui-org/react";
 import {Chip} from "@nextui-org/react";
 import React from "react";
 import axios from "axios";
+import {User} from "@nextui-org/user";
+import {Code} from "@nextui-org/react";
+
 
 export const FeatureMain = () =>{
     const [value, setValue] = React.useState("");
-    const [success, setSuccess] = React.useState(false);
+
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const postFeature = async() =>{
       if(value.length > 10){
         const response = await axios.post(backendUrl+ "/easyrepo/post/feature" , {"description": value} , {withCredentials: true , headers : {'X-CSRF-TOKEN': sessionStorage.getItem('csrf') }})
-        if(response?.data === "success"){
-          setSuccess(true)
-        }
+        
       }
         
     }
@@ -43,8 +44,32 @@ export const FeatureMain = () =>{
         Request
       </Button> 
     </div>
-    {success && <Chip color="warning" variant="dot" className="mt-3 ml-3">An email notification will be sent if your feature is implemented.</Chip>}
-    
+    <div>
+     <Chip color="warning" variant="dot" className="mt-6 ml-6"> 
+      We'll email you if implemented.</Chip>
+      </div>
+      <div>
+        <div className="mt-4 ml-4">
+      <Code color="default">Contributors</Code>
+      </div>
+      <User
+      avatarProps={{
+        src: "https://avatars.githubusercontent.com/u/184331957?s=400&u=b4b32d6e417646b36b6ace28b2bc0505e802770a&v=4",
+      }}
+      description="Developer"
+      name="Barsat Khadka"
+      className="ml-4 mt-3  "
+    />
+     <User
+      avatarProps={{
+        src: "",
+      }}
+      description= {<a href="" className="hover:underline">How to contribute</a>}
+      name="Be the next contributor"
+      className="ml-4 mt-3  "
+    />
+    </div>
+  
         </>
     )
 }
