50478ee925a52b86e5fa7330019f99dea57ed024
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-16 20:39:29 -0600

    update: Github Calendar load issue fixed

--- grms-frontend/src/components/HeroSection/HeroSectionAfterAuth.tsx
+++ grms-frontend/src/components/HeroSection/HeroSectionAfterAuth.tsx
@@ -1,22 +1,24 @@
-import GitHubCalendar from "react-github-calendar"
+import { useEffect, useState } from "react";
+import GitHubCalendar from "react-github-calendar";
 
-interface propType{
-    username: string
+interface propType {
+    username: string;
 }
 
-export const HeroSectionAfterAuth = (props : propType) =>{
-    return(
+export const HeroSectionAfterAuth = (props: propType) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    return (
         <>
-        <div>
-  <div key="item-1" className="col-start-1 row-start-1 col-span-3 mb-3 ml-4 ">
-  <GitHubCalendar username={props.username}/>
-  </div>
-  
-</div>
-         
-
-      
-         
+            <div>
+                <div key="item-1" className="col-start-1 row-start-1 col-span-3 mb-3 ml-4">
+                    {isMounted && <GitHubCalendar username={props.username} />}
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
